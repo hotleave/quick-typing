a816672dc1b60ed1e3377c0f536656d86439c01e
hotleave <hotleave@gmail.com>
2021-02-18 10:07:31 +0800

    修改Title

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
+const name = 'Quick Typing'
+
 module.exports = {
-  pwa: {
-    name: 'QuickTyping'
+  pwa: { name },
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = name
+      return args
+    })
   },
 
   productionSourceMap: false
